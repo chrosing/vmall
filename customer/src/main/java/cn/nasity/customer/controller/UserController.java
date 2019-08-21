a06b0322c704661e7d79ea13bcee97fe4fbf879b
kaisec <15178905077@163.com>
2019-08-22 00:07:10 +0800

    :wrench: 修复父子pom文件的引用问题 :heavy_minus_sign: 解决pom引用重复Jar包的问题

--- customer/src/main/java/cn/nasity/customer/controller/UserController.java
+++ customer/src/main/java/cn/nasity/customer/controller/UserController.java
@@ -2,7 +2,6 @@ package cn.nasity.customer.controller;
 
 import cn.nasity.common.entity.User;
 import cn.nasity.common.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
